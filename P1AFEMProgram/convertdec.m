24d40add90f40034646b34a208ddff71102edc1c
Stefanie Beuter <114648170+sbeuteruulm@users.noreply.github.com>
2022-11-11 16:55:28 +0100

    Add files via upload

---
+++ P1AFEMProgram/convertdec.m
@@ -0,0 +1,24 @@
+function A = convertdec(n,b)
+% converts integers 0,1,2,...,b^n-1 from decimal
+% to number system with base b (reverse order)
+%
+% A = convertdec(n,b) converts all natural numbers 0 to n to a number
+% system with base b. A is a matrix that contains the numbers 0 to n in
+% each row in ascending order in the converted number system.
+%
+% Remark:
+%   This program is a supplement to the paper 
+%   >> Efficient P1-FEM for any space dimension in Matlab <<
+%   by S. Beuter, and S. Funken. The reader should 
+%   consult that paper for more information.   
+%
+% Authors:
+%   S.Beuter, S. Funken 18-10-22
+
+A = zeros(b^n,n);
+idx = (0:b^n-1)';
+%for i=1:n
+for i = n:-1:1
+  A(:,i) = rem(idx,b);
+  idx = floor(idx/b);
+end
---
+++ P1AFEMProgram/refineNVBnD.m
@@ -0,0 +1,54 @@
+function [coordinates,elements,element2neighbour,level] = ...
+                           refineNVBnD(coordinates,elements,element2neighbour,...
+                           level,varargin)
+% This function gives a framework for the refinement process.
+%
+% [coordinates,elements,element2neighbour,level] = ...
+% refineNVBnD(coordinates,elements,element2neighbour,level,varargin)
+% updates a mesh by the newest vertex bisection. The mesh is described by
+% the arrays coordinates containing the coordinates for each vertex row
+% wise, the array elements that describes the simplices by their vertex
+% numbers, element2neighbour assembles for each element all its neighbours
+% in one row and vector level that gives the level of each simplex. These
+% arrays get enlarged and updated before, they are returned. The cell array
+% varargin assembles boundary arrays and as last element a vector with
+% element numbers that are marked for refinement.
+%
+% Comments:
+%   This function is called to perform the REFINE step of the adaptive FEM.
+%   All marked elements are bisected by the newest vertex bisection. To
+%   ensure that every marked element and neighbours sharing the refinement
+%   edge are bisected exactly once, the refine function is called if the
+%   global level has not been updated before. The refine function
+%   identifies all neighbours sharing a refinement edge and initiates the
+%   Newest Vertex Bisection.
+%
+% Remark:
+%   This program is a supplement to the paper 
+%   >> Efficient P1-FEM for any space dimension in Matlab <<
+%   by S. Beuter, and S. Funken. The reader should 
+%   consult that paper for more information.
+%
+% Authors:
+%   S. Beuter, S. Funken 18-10-22
+
+global C E L E2N i2fi nC nE  
+nB = length(varargin)-1;
+marked = varargin{end};
+
+E2N = element2neighbour;
+nE = size(elements,1); nC = size(coordinates,1);
+C = coordinates; E = elements; L = level; 
+%*** allocate memory for refined mesh
+nEmax = ceil(2.2 * nE);
+E(nEmax,1) = 0;  E2N(nEmax,1) = 0; L(nEmax,1) = 0; C(2*nC,1) = 0;
+%*** refine mesh by bisection
+lev = L;
+for e = reshape(marked,1,[])
+  if lev(e) == L(e)
+    refine(e);  
+  end
+end
+coordinates = C(1:nC,:); elements = E(1:nE,:);  level = L(1:nE); E2N = E2N(1:nE,:);
+element2neighbour = E2N;
+level = L;
